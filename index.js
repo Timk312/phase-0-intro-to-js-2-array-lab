c3a4d6043864099ad7b11b5eed3b3a63e835d06e
Tim Kingston <timkingston312@gmail.com>
2023-03-16 23:24:01 -0400

    arrayLab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats =['Milo','Otis','Garfield']
+function destructivelyAppendCat(){
+cats.push('Ralph');
+}
+function destructivelyPrependCat(){
+cats.unshift('Bob')  ; 
+}
+function destructivelyRemoveLastCat(){
+cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+const catsCopy=cats.slice()
+catsCopy.push('Broom');
+return catsCopy;
+}
+
+function prependCat(){
+const newCats=cats.slice();
+newCats.unshift('Arnold');
+return newCats;
+}
+
+function removeLastCat(){
+const newCats=cats.slice();
+newCats.pop();
+return newCats;
+}
+
+function removeFirstCat(){
+const newCats=cats.slice();
+newCats.shift();
+return newCats;
+}
